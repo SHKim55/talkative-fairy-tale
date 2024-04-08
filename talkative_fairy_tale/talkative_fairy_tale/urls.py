e0a7f9b30c4e37d3f9ef87a5c61477a7833052b2
Seonho Kim <5061ksh@naver.com>
2024-04-08 16:18:25 +0900

    feat : add mappings to write.html & several demo html pages

--- talkative_fairy_tale/talkative_fairy_tale/urls.py
+++ talkative_fairy_tale/talkative_fairy_tale/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),           # 장고 관리자 페이지
+    path('', views.home),                      # 메인 홈페이지 (서버 시작하자마자 들어가는 페이지)
+    path('story/', include('newstory.urls')),  # 스토리 생성/불러오기와 관련된 페이지
 ]
