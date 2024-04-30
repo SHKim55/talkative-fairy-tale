345fb2f6b5d5ec5ed86aec38cdf0fd9b2d5f72c5
Seonho Kim <5061ksh@naver.com>
2024-05-01 02:09:00 +0900

    Initial commit

---
+++ src/main/java/com/softgallery/talkativefairytale/TalkativeFairyTaleApplication.java
@@ -0,0 +1,13 @@
+package com.softgallery.talkativefairytale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkativeFairyTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkativeFairyTaleApplication.class, args);
+	}
+
+}
