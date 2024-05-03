647dd251758f01863195d99b75b60576caac6e75
Seonho Kim <5061ksh@naver.com>
2024-05-03 18:26:46 +0900

    refactor : add constructor

--- src/main/java/com/softgallery/talkativefairytale/dao/StoryDAO.java
+++ src/main/java/com/softgallery/talkativefairytale/dao/StoryDAO.java
@@ -1,8 +1,21 @@
 package com.softgallery.talkativefairytale.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StoryDAO {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public StoryDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("character")
+                .usingColumns("name", "gender", "personality_good", "personality_bad", "personality_normal")
+                .usingGeneratedKeyColumns("id_character");
+    }
+
 
 }
