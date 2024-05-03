dd27c1822bf4b47b8b53a4179ddcacf96087945d
Seonho Kim <5061ksh@naver.com>
2024-05-03 18:27:45 +0900

    refactor : add id field and extra constructors

--- src/main/java/com/softgallery/talkativefairytale/dto/UserDTO.java
+++ src/main/java/com/softgallery/talkativefairytale/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.softgallery.talkativefairytale.dto;
 
+import com.softgallery.talkativefairytale.domain.User;
+
 public class UserDTO {
+    private Long id;
     private String username;
 
     public UserDTO() { }
@@ -9,5 +12,16 @@ public class UserDTO {
         this.username = username;
     }
 
+    public UserDTO(final Long id, final String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public UserDTO(final User user) {
+        this.id = user.getId();
+        this.username = username;
+    }
+
+    public Long getId() { return id; }
     public String getUsername() { return username; }
 }
