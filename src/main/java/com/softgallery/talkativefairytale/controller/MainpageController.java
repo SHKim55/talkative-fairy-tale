6a0827a6176f096663199e5742ee6264ebef4996
Seonho Kim <5061ksh@naver.com>
2024-05-06 03:27:28 +0900

    feat : load (in)complete story list written by the user

--- src/main/java/com/softgallery/talkativefairytale/controller/MainpageController.java
+++ src/main/java/com/softgallery/talkativefairytale/controller/MainpageController.java
@@ -1,35 +1,47 @@
 package com.softgallery.talkativefairytale.controller;
 
-import com.softgallery.talkativefairytale.dto.CharacterDTO;
+import com.softgallery.talkativefairytale.dto.StoryDTO;
+import com.softgallery.talkativefairytale.dto.UserDTO;
 import com.softgallery.talkativefairytale.service.MainpageService;
 import com.softgallery.talkativefairytale.service.StoryMaking;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class MainpageController {
     private final MainpageService mainpageService;
     private final StoryMaking storyMaking;
+
+    private ArrayList<StoryDTO> completeStories = new ArrayList<>();
+    private ArrayList<StoryDTO> incompleteStories = new ArrayList<>();
 
     public MainpageController(MainpageService mainpageService, StoryMaking storyMaking) {
         this.mainpageService = mainpageService;
         this.storyMaking = storyMaking;
     }
 
-    @PostMapping("/character/insert")
-    public ResponseEntity<CharacterDTO> insertCharacterTest(@RequestBody CharacterDTO characterDTO) {
-        CharacterDTO newCharacter = storyMaking.insertNewCharacter(characterDTO);
-        return ResponseEntity.created(URI.create("/character/insert/" + newCharacter.getId())).body(newCharacter);
+    @PostMapping("/")
+    public ResponseEntity<Map<String, Object>> main(@RequestBody UserDTO userDTO) {
+        Map<String, Object> responseEntity = new HashMap<>();
+
+        // 여기서 페이지 로딩에 필요한 데이터들 다 불러오기
+        incompleteStories = mainpageService.findIncompleteStoriesMadeByUser(userDTO.getUsername());
+        completeStories = mainpageService.findCompleteStoriesMadeByUser(userDTO.getUsername());
+
+        // 한번에 Map으로 집어넣어서 보내기
+        responseEntity.put("incompleteStories", incompleteStories);
+        responseEntity.put("completeStories", completeStories);
+
+        return ResponseEntity.ok().body(responseEntity);
     }
 
-    @GetMapping("/character/find/id/{id}")
-    public ResponseEntity<CharacterDTO> readCharacterIdTest(@PathVariable Long id) {
-        CharacterDTO foundCharacter = storyMaking.findCharacterById(id);
-        return ResponseEntity.ok().body(foundCharacter);
-    }
 }
--- src/main/java/com/softgallery/talkativefairytale/service/MainpageService.java
+++ src/main/java/com/softgallery/talkativefairytale/service/MainpageService.java
@@ -2,6 +2,12 @@ package com.softgallery.talkativefairytale.service;
 
 import com.softgallery.talkativefairytale.dao.StoryDAO;
 import com.softgallery.talkativefairytale.dao.UserDAO;
+import com.softgallery.talkativefairytale.domain.Story;
+import com.softgallery.talkativefairytale.domain.User;
+import com.softgallery.talkativefairytale.dto.StoryDTO;
+import com.softgallery.talkativefairytale.dto.UserDTO;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +18,25 @@ public class MainpageService {
     public MainpageService(UserDAO userDAO, StoryDAO storyDAO) {
         this.userDAO = userDAO;
         this.storyDAO = storyDAO;
+    }
+
+    public ArrayList<StoryDTO> findIncompleteStoriesMadeByUser(String username) {
+        ArrayList<StoryDTO> storyDTOs = new ArrayList<>();
+        List<Story> stories = storyDAO.findIncompleteStoriesByName(username);
+
+        for(Story story : stories) {
+            storyDTOs.add(new StoryDTO(story));
+        }
+        return storyDTOs;
+    }
+
+    public ArrayList<StoryDTO> findCompleteStoriesMadeByUser(String username) {
+        ArrayList<StoryDTO> storyDTOs = new ArrayList<>();
+        List<Story> stories = storyDAO.findCompleteStoriesByName(username);
+
+        for(Story story : stories) {
+            storyDTOs.add(new StoryDTO(story));
+        }
+        return storyDTOs;
     }
 }
