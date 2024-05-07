86dddfd487976bf0d94d4c1a200e212aa5cb429e
Seonho Kim <5061ksh@naver.com>
2024-05-07 13:35:08 +0900

    add : class for storing base GPT prompting data

---
+++ src/main/java/com/softgallery/talkativefairytale/data/GPTPromptingInfo.java
@@ -0,0 +1,44 @@
+package com.softgallery.talkativefairytale.data;
+
+public class GPTPromptingInfo {
+    private final String initalizingMessage = "차근차근 생각해 보자.\n"
+            + "너는 어린 아이를 위한 동화 작가야. 나는 5~7세 사이 너의 동화를 읽는 아동이야.\n"
+            + "내가 동화에 등장하는 등장인물들을 알려주면, 너는 동화의 시작 부분을 작성해 주면 돼. 동화의 시작 부분에는 등장인물이나 배경에 대한 소개가 들어가야 해. 등장 인물간의 관계도 있어야 해. 등장인물의 소개는 간결해야 해. 시작 부분도 동화의 한 부분이기 때문에 어린아이가 이해하기 쉬운 단어를 사용해야 하고 뒤에 동화가 이어질 것을 고려해야 해. 너는 높임말을 써야 하고, 동화의 흐름은 최고로 자연스러워야 해. 반드시 쉬운 단어만 사용해야 해. 문장은 짧을 수록 좋아.\n"
+            + "\n"
+            + "너의 임무는 일관된 스타일로 동화의 도입부를 작성하는 거야.";
+
+    private final String koreanStoryFormat = "<line>옛날옛적, 어느 작은 마을에 아주 사이 좋은 형제가 살고 있었어요.</line>\n"
+            + "<line>형은 결혼하여 가족들과 살고 있었고, 동생은 어머니를 모시고 있었지요.</line>\n"
+            + "<line>두 형제는 매일 들에 나가 열심히 일을 했어요.</line>\n"
+            + "<line>옛날 옛날에 한 마을에 모두의 사랑을 받는 작고 귀여운 소녀가 있었습니다.</line>\n"
+            + "<line>그 소녀를 가장 사랑하는 것은 그녀의 할머니였습니다.</line>\n"
+            + "<line>하루는 할머니가 소녀에게 붉은 벨벳으로 만들어진 모자를 선물했습니다.</line>\n"
+            + "<line>소녀에게 그 모자가 잘 어울렸고, 소녀가 그 모자가 아닌 다른 것은 쓰지 않으려고 했습니다.</line>\n"
+            + "<line>그래서 그 소녀는 '빨간 모자'라고 불렸습니다.</line>";
+
+    private final String adaptingKoreanStyleMessage = "너는 동화 작가야.\n"
+            + "앞의 문장을 한국의 동화 스타일로 바꿔야 해.\n"
+            + "한국 동화의 스타일은 다음과 같아.\n"
+            + koreanStoryFormat;
+
+    private final String storyContinuingMessage = "너는 5~7세 사이 아동을 위한 동화를 짓는 동화 작가야. "
+            + "나는 너의 이야기를 읽는 어린이 독자야. 너는 지금까지의 이야기를 보고 다음 이야기를 한 문장 만들어 주면 돼. "
+            + "이야기의 흐름은 최고로 자연스러워야 하고, 아이들이 이해하기 쉬운 단어로 문장을 구성해야 해. "
+            + "또한 너는 한국어를 써야하고 높임말을 사용해야 해.";
+
+    public GPTPromptingInfo() { }
+
+    public String getInitalizingMessage() { return this.initalizingMessage; }
+
+    public String getStyleOptimizingMessage() {
+        return this.adaptingKoreanStyleMessage;
+    }
+
+    public String getStoryContinuingMessage(String topic, Long level, String prevStory) {
+        String result = this.storyContinuingMessage + "\n\n"
+                + "이야기의 주제는 " + topic + " 이고, "
+                + "지금까지 만들어진 이야기는 다음과 같아\n\n"
+                + prevStory;
+        return result;
+   }
+}
