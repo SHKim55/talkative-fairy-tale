2afbd3ec8f8ddfe13adfb8393baad85ba95ce06e
Seonho Kim <5061ksh@naver.com>
2024-05-09 19:02:10 +0900

    feat : add gpt token calling logic

--- src/main/java/com/softgallery/talkativefairytale/controller/ChatGptController.java
+++ src/main/java/com/softgallery/talkativefairytale/controller/ChatGptController.java
@@ -3,6 +3,9 @@ package com.softgallery.talkativefairytale.controller;
 import com.softgallery.talkativefairytale.dto.ChatGptResponseDTO;
 import com.softgallery.talkativefairytale.dto.QuestionRequestDTO;
 import com.softgallery.talkativefairytale.service.ChatGptService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,9 @@ public class ChatGptController {
 
     private final ChatGptService chatGptService;
 
+    @Value("${chatgpt.api-key}")
+    private String gptToken;
+
     public ChatGptController(ChatGptService chatGptService) {
         this.chatGptService = chatGptService;
     }
@@ -21,5 +27,10 @@ public class ChatGptController {
     @PostMapping("/question")
     public ChatGptResponseDTO sendQuestion(@RequestBody QuestionRequestDTO requestDTO) {
         return chatGptService.askQuestion(requestDTO);
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<String> getToken() {
+        return ResponseEntity.ok().body(this.gptToken);
     }
 }
