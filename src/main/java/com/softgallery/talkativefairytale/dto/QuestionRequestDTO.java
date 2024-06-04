9c596cce19ca78dab0f2517c74186542effa30ed
1Kirl <165295500+1Kirl@users.noreply.github.com>
2024-06-04 20:19:34 +0900

    [Feat#37] Transfer GPT to Server (#39)
    * feat: transfer gpt to server
    
    * fix: add application.properties in .gitignore

--- src/main/java/com/softgallery/talkativefairytale/dto/QuestionRequestDTO.java
+++ src/main/java/com/softgallery/talkativefairytale/dto/QuestionRequestDTO.java
@@ -3,14 +3,15 @@ package com.softgallery.talkativefairytale.dto;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Getter
 public class QuestionRequestDTO implements Serializable {
-    private String question;
+    private List<Message> messages;
 
     public QuestionRequestDTO() { }
 
-    public QuestionRequestDTO(final String question) {
-        this.question = question;
+    public QuestionRequestDTO(List<Message> messages) {
+        this.messages = messages;
     }
 }
