eea4aa0f5968e712f040db12939caca4f0350003
1Kirl <165295500+1Kirl@users.noreply.github.com>
2024-06-11 16:32:11 +0900

    feat: change gpt config

--- src/main/java/com/softgallery/talkativefairytale/config/ChatGptConfig.java
+++ src/main/java/com/softgallery/talkativefairytale/config/ChatGptConfig.java
@@ -17,8 +17,8 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String MODEL = "gpt-3.5-turbo";
-    public static final Integer MAX_TOKEN = 300;
+    public static final String MODEL = "gpt-4o";
+    public static final Integer MAX_TOKEN = 4000;
     public static final Double TEMPERATURE = 0.0;
     public static final Double TOP_P = 1.0;
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
